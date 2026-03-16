377b5cc60823f2587ac5ca46d6a58ad59a1be103
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2026-03-17 00:07:15 +0100

    Database logger serviec to log the manual input

--- src/main/java/ch/fhnw/students/DatabaseLogger.java
+++ src/main/java/ch/fhnw/students/DatabaseLogger.java
@@ -1,10 +1,7 @@
 package ch.fhnw.students;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Map;
 
 public class DatabaseLogger {
@@ -12,27 +9,23 @@ public class DatabaseLogger {
 
     private static final String DATABASE_URL = "jdbc:mysql://192.168.111.4:3306/db_group2";
     private static final Map<String, String> env = System.getenv();
+    private static Connection connection;
 
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(DATABASE_URL, env.get("MYSQL_USER"), env.get("MYSQL_PASSWORD"));){
-            Statement statement = connection.createStatement();
-
-
-//          Code for testing the connection
-//          statement.execute("CREATE TABLE IF NOT EXISTS test (" +
-//                    "id INT PRIMARY KEY NOT NULL," +
-//                    "client_name VARCHAR(255) NOT NULL)");
-
-            // SQL Code to create the table if it doesn't exist in the DB
-            dbInit(statement);
-            connection.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
 
     }
 
-    public static void dbInit(Statement statement) throws SQLException {
+    public static void createConnection() {
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, env.get("MYSQL_USER"), env.get("MYSQL_PASSWORD"));
+            // SQL Code to create the table if it doesn't exist in the DB
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void dbInit() throws SQLException {
+        Statement statement = connection.createStatement();
         statement.execute("CREATE TABLE IF NOT EXISTS logging_group2_case2 (\n" +
                 "\tlogging_id INT PRIMARY KEY auto_increment,\n" +
                 "    order_id VARCHAR(255) NOT NUll,\n" +
@@ -41,5 +34,19 @@ public class DatabaseLogger {
                 "    shipping_weight DOUBLE NOT NULL,\n" +
                 "    delivery_type VARCHAR(255) NOT NULL\n" +
                 ");");
+    }
+
+    public static void logging(String orderId, String customerId, String destination, double weight, String deliveryType) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO logging_group2_case2 (" +
+                "order_id, customer_id, destination_country, shipping_weight, delivery_type) VALUES (" +
+                "?,?,?,?,?)");
+
+        ps.setString(1, orderId);
+        ps.setString(2, customerId);
+        ps.setString(3, destination);
+        ps.setDouble(4, weight);
+        ps.setString(5, deliveryType);
+
+        ps.execute();
     }
 }
