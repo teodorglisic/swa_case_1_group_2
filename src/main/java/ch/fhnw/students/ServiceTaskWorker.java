d740c951689a43afc4bc7bfe3b3143757c376a91
Teodor Glisic <teodor.glisic@students.fhnw.ch>
2026-03-17 00:07:34 +0100

    ServicetaskWorker with new handler for the mysql db logging

--- src/main/java/ch/fhnw/students/ServiceTaskWorker.java
+++ src/main/java/ch/fhnw/students/ServiceTaskWorker.java
@@ -3,6 +3,7 @@ package ch.fhnw.students;
 import org.camunda.bpm.client.ExternalTaskClient;
 import org.json.JSONObject;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -26,7 +27,7 @@ public class ServiceTaskWorker {
                     String country = (String) externalTask.getVariable("deliveryCountry");
 
                     JSONObject courierAPIResult = ApiRequester.selectCourierAPI(country, weight);
-
+                    System.out.println(courierAPIResult);
                     HashMap<String, Object> result = new HashMap<>();
                     int statusCode = courierAPIResult.getInt("statusCode");
                     if (statusCode == 202) {
@@ -36,13 +37,11 @@ public class ServiceTaskWorker {
                         result.put("deliveryCheck", courierAPIResult.getString("deliveryType"));
                     } else {
                        result.put("statusCode", statusCode);
-                       if (Objects.equals(courierAPIResult.getString("destination"), "NOT_DEFINED")) {
+                       if (!Objects.equals(courierAPIResult.getString("destination"), "NOT_DEFINED")) {
                            result.put("destination", courierAPIResult.getString("destination"));
                        } else {
                            result.put("destination", courierAPIResult.getString("deliveryCountryManualCheck"));
                        }
-                       System.out.println((Long) externalTask.getVariable("weight"));
-                       result.put("destination", courierAPIResult.getString("deliveryCountryManualCheck"));
                        result.put("deliveryCheck", courierAPIResult.getString("deliveryType"));
 
                     }
@@ -94,9 +93,32 @@ public class ServiceTaskWorker {
 
 
         // Service task für Protokollierung in SQL
-        client.subscribe("group2_sqlProtocolling").lockDuration(1000).handler(
+        client.subscribe("group2_sqlLogging").lockDuration(1000).handler(
                 (externalTask, externalTaskService) -> {
 
+                    String orderID = (String) externalTask.getVariable("orderId");
+                    String customerId = (String) externalTask.getVariable("customerId");
+                    String destination = (String) externalTask.getVariable("destination");
+                    Long weight = (Long) externalTask.getVariable("weight");
+                    String deliveryType = (String) externalTask.getVariable("deliveryType");
+
+                    System.out.println(orderID);
+                    System.out.println(customerId);
+                    System.out.println(destination);
+                    System.out.println(weight);
+                    System.out.println(deliveryType);
+
+
+                    try {
+                        DatabaseLogger.createConnection();
+                        DatabaseLogger.dbInit();
+                        DatabaseLogger.logging(orderID, customerId, destination, weight, deliveryType);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+
+
+                    externalTaskService.complete(externalTask);
         }).open();
     }
 
